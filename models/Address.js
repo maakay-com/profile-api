0b357011a2119eb447fed8f98fcdb67419441f49
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-03-21 17:46:13 +0545

    added timestamps

--- models/Address.js
+++ models/Address.js
@@ -1,24 +1,29 @@
 const mongoose = require('mongoose');
 
 
-const AddressSchema = mongoose.Schema({
-    address: {
-        type: String,
-        required: true,
-        minLength: 24,
-        maxLength: 128
+const AddressSchema = mongoose.Schema(
+    {
+        address: {
+            type: String,
+            required: true,
+            minLength: 24,
+            maxLength: 128
+        },
+        metadata: String,
+        token: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Token',
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    metadata: String,
-    token: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Token',
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    {
+        timestamps: true
     }
-})
+)
 
 
 module.exports = mongoose.model('Address', AddressSchema)
--- models/User.js
+++ models/User.js
@@ -2,18 +2,23 @@ const mongoose = require('mongoose');
 const generateNonce = require('../utils/generate_nonce');
 
 
-const UserSchema = new mongoose.Schema({
-    account_number: {
-        type: String,
-        required: true,
-        minLength: 64,
-        maxLength: 64
+const UserSchema = new mongoose.Schema(
+    {
+        account_number: {
+            type: String,
+            required: true,
+            minLength: 64,
+            maxLength: 64
+        },
+        nonce: {
+            type: Number,
+            default: generateNonce()
+        },
     },
-    nonce: {
-        type: Number,
-        default: generateNonce()
+    {
+        timestamps: true
     }
-})
+)
 
 
 module.exports = mongoose.model('User', UserSchema)
