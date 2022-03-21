1960295987ee1281b4970ff3a8ddb2943cad8162
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-03-21 17:45:34 +0545

    define Profile schema

---
+++ models/Profile.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+const ProfileSchema = mongoose.Schema(
+    {
+        username: String,
+        description: {
+            type: String,
+            default: 'Awesome description.'
+        },
+        avatar_url: {
+            type: String,
+            default: 'https://picsum.photos/200'
+        },
+        is_verified: {
+            type: Boolean,
+            default: false
+        },
+        subscription_type: {
+            type: String,
+            enum: ['IRON', 'GOLD', 'DIAMOND'],
+            default: 'IRON'
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Profile', ProfileSchema)
