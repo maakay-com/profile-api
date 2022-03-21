2f8edf2309c5c57c98f0a29c819ad4aee654abeb
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-03-21 18:57:04 +0545

    implement GET /profile/:username endpoint

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,22 @@
 const { profile } = require('../data')
+const Profile = require('../models/Profile')
+const Address = require('../models/Address')
 
 
-const getProfile = (req, res) => {
-    const { username } = req.params;
-    const singleUser = profile.find(
-        (user) => user.username === username
-    )
+const getProfile = async (req, res) => {
 
-    if (!singleUser) {
-        return res.status(404).json({'detail': 'Not Found'})
+    try {
+        const { username } = req.params;
+        const profile = await Profile.findOne({ username: username })
+
+        if (profile) {
+            var addresses = await Address.find({ user: profile.user })
+        }
+        return res.json({ profile, addresses })
+
+    } catch (err) {
+        return res.json(err)
     }
-
-    return res.json(singleUser)
 }
 
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const router = express.Router();
+const auth = require('../middleware/auth')
 const {
     getProfile,
     updateProfile
 } = require('../controllers/profile')
 
 
-router.route('/:username').get(getProfile).patch(updateProfile)
+router.route('/:username').get(getProfile);
+router.use('/', auth);
+router.route('/').patch(updateProfile);
 
 
 module.exports = router;
