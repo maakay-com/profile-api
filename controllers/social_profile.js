dc55e3d18c23e56620d7af7be0746ef103e7d684
Noor Ahmad <n.ahmad0093@gmail.com>
2022-03-27 16:16:28 +0300

    socail profile routes are done without test

--- controllers/social_profile.js
+++ controllers/social_profile.js
@@ -1,29 +1,55 @@
-const { social_profile } = require("../data");
+const {social_profile} = require('../data')
+const SocialProfile = require('../models/SocialProfile')
 
-const getAllSocialProfile = (req, res) => {
-  res.json(social_profile);
-};
+const getAllSocialProfile = async (req, res) => {
+    const allProfiles = await SocialProfile.find()
+    res.json(allProfiles)
+}
 
-const getSocialProfile = (req, res) => {
-  res.json(social_profile);
-};
+const getSocialProfile = async (req, res) => {
+    const profile = await SocialProfile.findById(req.param.id)
+    if (!profile) {
+        throw new Error(`profile not found!`) // A custom Error should be implemented!
+    }
+    res.json(profile)
+}
 
-const createSocialProfile = (req, res) => {
-  res.json(social_profile);
-};
+const createSocialProfile = async (req, res) => {
+    // TODO: ask about the response and body!!
+    const userId = req.body.user_id
+    const socialMedia = req.body.social_media_id
+    const username = req.body.username
+    const profile = await SocialProfile.create({
+        user: userId,
+        socialMedia: socialMedia,
+        username: username,
+    })
+    res.json(profile)
+}
 
-const updateSocialProfile = (req, res) => {
-  res.json(social_profile);
-};
+const updateSocialProfile = async (req, res) => {
+    const profile = await SocialProfile.findById(req.params.id)
+    if (!profile) {
+        throw new Error(`profile not found!`) // A custom Error should be implemented!
+    }
+    profile.username = req.body.username || profile.username
+    profile.user = req.body.userId || profile.user
+    profile.socialMedia = req.body.social_media_id || profile.socialMedia
+    res.json(await profile.save())
+}
 
-const deleteSocialProfile = (req, res) => {
-  res.json(social_profile);
-};
+const deleteSocialProfile = async (req, res) => {
+    const profile = await SocialProfile.deleteOne({_id: req.params.id})
+    if (!profile) {
+        throw new Error(`profile not found!`) // A custom Error should be implemented!
+    }
+    res.json(profile)
+}
 
 module.exports = {
-  getAllSocialProfile,
-  getSocialProfile,
-  createSocialProfile,
-  updateSocialProfile,
-  deleteSocialProfile,
-};
+    getAllSocialProfile,
+    getSocialProfile,
+    createSocialProfile,
+    updateSocialProfile,
+    deleteSocialProfile,
+}
--- db/localDb.js
+++ db/localDb.js
@@ -1,48 +1,46 @@
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require('mongoose')
+const {MongoMemoryServer} = require('mongodb-memory-server')
 
-let mongoServer;
+let mongoServer
 
 // For mongodb-memory-server's old version (< 7) use this instead:
 // const mongoServer = new MongoMemoryServer();
 
 const opts = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
 
 // Provide connection to a new in-memory database server.
 const connect = async () => {
-  // NOTE: before establishing a new connection close previous
-  await mongoose.disconnect();
-
-  mongoServer = await MongoMemoryServer.create();
-
-  const mongoUri = await mongoServer.getUri();
-  await mongoose.connect(mongoUri, opts, (err) => {
-    if (err) {
-      console.error(err);
+    try {
+        // NOTE: before establishing a new connection close previous
+        await mongoose.disconnect()
+        mongoServer = await MongoMemoryServer.create()
+        const mongoUri = mongoServer.getUri()
+        await mongoose.connect(mongoUri)
+    } catch (error) {
+        console.log(error)
     }
-  });
-};
+}
 
 // Remove and close the database and server.
 const close = async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
-};
+    await mongoose.disconnect()
+    await mongoServer.stop()
+}
 
 // Remove all data from collections
 const clear = async () => {
-  const collections = mongoose.connection.collections;
+    const collections = mongoose.connection.collections
 
-  for (const key in collections) {
-    await collections[key].deleteMany();
-  }
-};
+    for (const key in collections) {
+        await collections[key].deleteMany()
+    }
+}
 
 module.exports = {
-  connect,
-  close,
-  clear,
-};
+    connect,
+    close,
+    clear,
+}
